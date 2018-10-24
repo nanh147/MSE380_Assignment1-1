232e28e5b748056db9f7e20ff7d0728c107f3ee8
Nam Anh Mai <32988140+nanh147@users.noreply.github.com>
2018-10-24 11:45:21 -0700

    Update MSE380_A1_1d_part_b.m

--- MSE380_A1_1d_part_b.m
+++ MSE380_A1_1d_part_b.m
@@ -7,17 +7,17 @@ b=20;
 k=500;
 k_theta = 1000;
 b_theta = 80;
-p=7874;
-L=0.15;
-r=0.75;
-h=0.1;
+p=7874; %%densityj = 0.5*(p*h*pi*(r^2))*(r^2);
+L=0.15; %%mass element side length
+r=0.75; %%inertia radius
+h=0.1; %%inertia thickness
 m = p*L*L*L;
-j = 0.5*m*r^2;
+j = 0.5*(p*h*pi*(r^2))*(r^2); %%use mass of inertia
 %% system matrices
 %%input
-a = [0 1 0 0; -k/m -b/m 0 0 ; 0 0 0 1; -k*r/j -b*r/j -k_theta/j -b_theta/j];
-b1 = [0; 1/m; 0; r/j];
-b2 = [0; 1/m; 0; r/j];
+a = [0 1 0 -r; -k/m -b/m 0 0; 0 0 0 1; k*r/j b*r/j -k_theta/j -b_theta/j];
+b1 = [0; 1/m; 0; 0];
+b2 = [0; 1; 0; m*r/j];
 %%output
 c = [1 0 0 0; 0 0 1 0];
 d1 = [0;0];
@@ -27,10 +27,9 @@ n=15/dt;
 x = zeros(4,n);
 x_diff = [0;0;0;0];
 y = zeros(2,n);
-ft=[];
-ft(1:1,1:n+1)=0;
-ft(1,1)=100;
-g(1:1,1:n+1) = 9.81;
+ft(1,1:(n+1))=0;
+ft(1,1:(n+1))=100;
+g(1,1:(n+1)) = 9.81;
 i=1;
 for t = 0:dt:15
     x_diff = a*x(:,i) + b1.*ft(:,i) + b2.*g(:,i);
@@ -41,7 +40,7 @@ end
 %% plot
 figure;
 plot(t_mat,y(1,:),t_mat,y(2,:));
-title('Euler Method State Models Part b');
+title('Euler Method State Model Part b');
 xlabel('Time (s)');
 ylabel('Displacement (m)');
 legend('Linear displacement','Angular displacement');
